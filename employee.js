93b634607ba756f25fe7ffc0401b0ecdf2c83840
dac <dac@dypm.org>
2019-06-26 10:00:20 +0530

    first commit

---
+++ employee.js
@@ -0,0 +1,13 @@
+var express= require("express")
+var app=express();
+var employee= [ {name:"Umang",country:"India",age:"22"},
+{name:"Kaustubh",country:"China",age:"23"},
+{name:"Devesh",country:"America",age:"24"},
+{name:"Dilip",country:"Bahrain",age:"25"}];
+
+var employeeDetails=function(req,res)
+{
+	res.send(employee)
+}
+app.get('/emp',employeeDetails);
+app.listen(8000);
