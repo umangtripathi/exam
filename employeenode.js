3492dbee399b1fdff2cc8fa9ee6579ab1b999625
dac <dac@dypm.org>
2019-06-26 11:14:39 +0530

    first commit

--- employeenode.js
+++ employeenode.js
@@ -1,8 +1,8 @@
 var http=require("http")
-var employee= [ {name:"Umang",country:"India",age:"22"},
-{name:"Kaustubh",country:"China",age:"23"},
-{name:"Devesh",country:"America",age:"24"},
-{name:"Dilip",country:"Bahrain",age:"25"}];
+var employee= [ {name:"Umang",country:"India",age:"22",businessEntity:"Employee"},
+{name:"Kaustubh",country:"China",age:"23",businessEntity:"Employee"},
+{name:"Devesh",country:"America",age:"24",businessEntity:"Employee"},
+{name:"Dilip",country:"Bahrain",age:"25",businessEntity:"Employee"}];
 
 http.createServer(function(req,resp){
 resp.writeHead(200,{"Content-Type":"text/plain"});
